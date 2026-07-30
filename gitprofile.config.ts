a4c6a6232b6136db5f803891bbd2fe10d57233af
RAMUL <flyingbayblade@gmail.com>
2026-07-30 09:10:54 +0800

    Update site title to Portfolio of Renuel Gonzalves

--- gitprofile.config.ts
+++ gitprofile.config.ts
@@ -51,7 +51,7 @@ const CONFIG = {
       ],
     },
   },
-  seo: { title: 'Portfolio of Ariful Alam', description: '', imageURL: '' },
+  seo: { title: 'Portfolio of Renuel Gonzalves', description: '', imageURL: '' },
   social: {
     linkedin: 'ariful-alam',
     x: 'arif_szn',
